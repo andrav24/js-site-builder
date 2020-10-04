4efeb77d3b5f3af5e4b9658cc8e609d78324842c
andrav24 <andrav24@yandex.ru>
2020-10-04 12:42:29 +0700

    add css object

--- src/model.js
+++ src/model.js
@@ -1,7 +1,18 @@
+import image from './assets/image.jpg'
+
 export const model = [
   {
     type: 'heading',
     value: 'Hello world!!!!!',
+    options: {
+      tag: 'h2',
+      styles: {
+        background: 'linear-gradient(to right, #ff0099, #493240)',
+        color: '#fff',
+        'text-align': 'center',
+        padding: '1.5rem',
+      }
+    }
   },
   {
     type: 'paragraph',
@@ -17,6 +28,6 @@ export const model = [
   },
   {
     type: 'image',
-    value: './assets/image.jpg'
+    value: image
   }
 ]
--- src/templates.js
+++ src/templates.js
@@ -1,46 +1,32 @@
+import {row, col, css} from './utils'
+
 /*
   pure function
   т.е. зависят только от параметров и не зависят от глобального контекста.
   Это концепт из функционального программирования.
  */
 function heading(block) {
-  return `
-    <div class="row">
-      <div class="col-sm">
-        <h1>${block.value}</h1>
-      </div>
-    </div>
-  `
+  /*const tag = block.options.tag ?? 'h1'
+  const styles = block.options.styles ?? ''*/
+  const {tag = 'h1', styles} = block.options
+  return row(col(`<${tag}>${block.value}</${tag}>`), css(styles))
 }
 
 // pure function
 function paragraph(block) {
-  return `
-      <div class="row">
-        <div class="col-sm">
-          <p>${block.value}</p>
-        </div>
-      </div>
-    `
+  return row(col(`<p>${block.value}</p>`))
 }
 
 // pure function
 function columns(block) {
-  const html = block.value.map(item => `<div class="col-sm">${item}</div>`)
-  return `
-    <div class="row">
-      ${html.join('')}
-    </div>
-  `
+  //const html = block.value.map(item => col(item))
+  const html = block.value.map(col).join('')
+  return row(html)
 }
 
 // pure function
 function image(block) {
-  return `
-    <div class="row">
-      <img src="${block.value}" />
-    </div>
-  `
+  return row(`<img src="${block.value}" />`)
 }
 
 // т.к. имена полей совпадают со значениями, то можно записать в таком виде
